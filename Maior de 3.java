2c8ab408276cfae66d74bcb904e63ffb3a3f0a05
francisc011 <32169035+francisc011@users.noreply.github.com>
2018-01-26 16:04:35 +0000

    Add files via upload

--- Maior de 3.java
+++ Maior de 3.java
@@ -1,31 +1,37 @@
-package Pacote1;
 
 import java.util.Scanner;
-
-public class Maiorde3 {
-
-	public static void main(String[] args) {
-		Scanner dados = new Scanner (System.in);
-		double n1, n2, n3, maior;
-		//Variaveis
-		System.out.println("Qual o primeiro numero?");
-		n1 = dados.nextDouble();
-		System.out.println("Qual o segundo numero?");
-		n2 = dados.nextDouble();
-		System.out.println("Qual o terceiro numero?");
-		n3 = dados.nextDouble();
-		
-		if (n1>n2)
-			maior = n1;
-		else if (n2>n3)
-			maior = n2;
-		else 
-			maior = n3;
-		System.out.print("Maior = " + maior);
-		
-				
-		
-
-	}
-
-}
+public class maiorde3 {
+    static int maiorde3 (int X, int Y, int Z)
+    {if (X < Y)
+        X = Y;
+     if (X < Z)
+         X = Z;
+    return X;}
+    static int menorde3 (int X, int Y, int Z)
+    {if (X > Y)
+        X = Y;
+     if (X > Z)
+         X = Z;
+    return X;}
+    public static void main (String [] args) throws Exception{
+        int x, y ,z,a,b,c;
+        Scanner LerS = new Scanner(System.in);
+        System.out.print("Digite o primeiro valor (x) ");
+        x = LerS.nextInt();
+        System.out.print("Digite o segundo valor (y) ");
+        y = LerS.nextInt();
+        System.out.print("Digite o terceiro valor (z) ");
+        z = LerS.nextInt();
+        System.out.println("Menor = "+ menorde3 (x, y, z));       
+        
+      
+        System.out.print("Digite o primeiro valor (a) ");
+        a = LerS.nextInt();
+        System.out.print("Digite o segundo valor (b) ");
+        b = LerS.nextInt();
+        System.out.print("Digite o terceiro valor (c) ");
+        c = LerS.nextInt();
+        System.out.println("Menor = "+ menorde3 (a, b, c));
+    }
+    }
+    
