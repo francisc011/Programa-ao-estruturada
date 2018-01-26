3cf33bac68bdaee2ddbb2e18053344e4cc4bc6e1
francisc011 <32169035+francisc011@users.noreply.github.com>
2018-01-26 16:12:13 +0000

    Add files via upload

---
+++ Menorde3.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class maiorde3 {
+    
+    static int menorde3 (int X, int Y, int Z)
+    {if (X > Y)
+        X = Y;
+     if (X > Z)
+         X = Z;
+    return X;}
+    public static void main (String [] args) throws Exception{
+        int x, y ,z,a,b,c;
+        Scanner LerS = new Scanner(System.in);
+        System.out.print("Digite o primeiro valor (x) ");
+        x = LerS.nextInt();
+        System.out.print("Digite o segundo valor (y) ");
+        y = LerS.nextInt();
+        System.out.print("Digite o terceiro valor (z) ");
+        z = LerS.nextInt();
+        System.out.println("Menor = "+ menorde3 (x  , y, z));       
+       
+    }
+    }
+    
